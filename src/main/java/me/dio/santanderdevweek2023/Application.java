e8fd9314ee242d955b10609cbcfabc38a7439b9a
AninhaPardini <aninhapardini.work@gmail.com>
2023-10-22 16:12:05 -0300

    first commit

---
+++ src/main/java/me/dio/santanderdevweek2023/Application.java
@@ -0,0 +1,27 @@
+package me.dio.santanderdevweek2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal da aplicação
+ * 
+ * As dependências desse projeto são H2, Spring Web, Spring Data JPA e Spring
+ * Boot DevTools
+ * 
+ * Para acessar a memória do banco de dados, acesse
+ * http://localhost:8080/h2-console
+ * 
+ * Para acessar a API, acesse http://localhost:8080/bootcamp/swagger-ui.html
+ * 
+ * @author AninhaPardini
+ */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
